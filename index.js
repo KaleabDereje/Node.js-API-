5419e3b5f51a479af6f59b68c9fb116e75ff56a4
KaleabDereje <kaleabdereje388@gmail.com>
2024-08-21 21:57:01 +0300

    single product API retrieve by id

--- index.js
+++ index.js
@@ -12,7 +12,32 @@ app.get('/',(req, res)=>{
     res.send("Hello from Node API Server nodemon is the captain");
 });
 
-//make the API of the Product
+
+//make the API to view Products
+app.get('/api/products', async (req, res) => {
+
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products);
+    }
+    catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+
+//retrieve the products by their id
+app.get('/api/products/:id', async (req, res) =>{
+
+    try{
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    }
+    catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
 
 
 app.post('/api/products', async (req, res) => {
